ec19c67b348fe8d0b7faab4b4ff4bcdb78457e71
anastasiab <kim.ana@tuta.io>
2026-09-16 22:17:40 -0600

    assignment1

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function StudentInfo () {
+    return (
+    <div>
+        <h1>Anastasia Barter</h1>
+        <p><Link href="https://github.com/shorthippo/cprg306-assignments.git">GitHub Repo</Link></p>
+    </div>
+    )
+}
