5318b20d50b75b6397edc4df852e50b4725789a6
anastasiab <kim.ana@tuta.io>
2026-09-25 11:46:16 -0600

    playing with styling

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,8 +1,8 @@
 export default function Item({ item }) {
     const { name, quantity, category } = item;
     return (
-        <li className="flex flex-col items-center justify-center p-2 bg-emerald-800/50 w-75 text-center border-2 border-black mb-4">
-            <h2 className="font-bold text-2xl">{name}</h2>
+        <li className="flex flex-col items-center justify-center p-1 bg-emerald-800/50 w-75 text-center border-2 border-emerald-900/90 rounded-sm">
+            <h2 className="font-bold text-base">{name}</h2>
             <p>Buy: {quantity} in {category}</p>
         </li>
     )
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,8 +2,8 @@ import ItemList from "./item-list";
 
 export default function Page() {
   return (
-    <main className="min-h-screen bg-black flex flex-col items-center p-8">
-      <h1 className="text-4xl font-bold text-white mb-8">Shopping List</h1>
+    <main className="min-h-screen bg-emerald-100 flex flex-col items-center p-8">
+      <h1 className="text-4xl font-bold text-emerald-800 mb-8">Shopping List</h1>
       <ItemList />
     </main>
   );
